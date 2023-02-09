8fede69e357dedd98c0926640d53e26641da1b3b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-09 13:30:18 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeWageComputation1;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to EmployeeWage ComputationProgram");
+            EmployeeCheck.CheckEmployee();
+        }
+    }
+}
