eec465ccad8371af1f430171782c2060cc865210
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-10 10:20:52 +0530

    Check Employee Max Work Days in Month

---
+++ EmplyeeMaxHrs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation1
+{
+    public class EmplyeeMaxHrs
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int EMPLOYEE_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 2;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
+        public static void CheckEmployeeMaxWork()
+        {
+
+
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+           while (totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+
+                Console.WriteLine("Day # : " + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMPLOYEE_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
